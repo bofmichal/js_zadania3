8fbabaac72dea627bc2117ff487a14c313ad55ad
bofmichal <33184065+bofmichal@users.noreply.github.com>
2018-04-14 21:45:56 +1000

    zad1

--- newjs/newjs.js
+++ newjs/newjs.js
@@ -1,1 +1,37 @@
 // zad 1
+function tab1(){
+    var ar=[];
+    var str="";
+ var los;
+  for (var i = 0; i < 20; ++i) {
+     los=(Math.floor(Math.random()*10)+1);
+     str += los + ", ";
+     ar[i]=" "+(Math.floor(Math.random()*10)+1);
+}
+document.getElementById("wypis1").innerHTML=ar;
+}
+function tab2(){
+    var ar=[];    
+   for (var i = 0; i < 20; ++i) {    
+     ar[i]=-1-i;
+}
+document.getElementById("wypis2").innerHTML=ar;
+}
+
+function tab3(){
+    var ar=[];    
+   for (var i = 0; i < 21; ++i) {    
+     ar[i]=" "+i/20;
+}
+document.getElementById("wypis3").innerHTML=ar;
+}
+
+function tab4(){
+    var ar=[];    
+   for (var i = 0; i < 20; ++i) {    
+     ar[i]=Math.pow(2,(i+1));;
+}
+document.getElementById("wypis4").innerHTML=ar;
+}
+
+//zad 2
