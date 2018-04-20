936c5d344c7e29cc6a643dcc498f9717b428d044
bofmichal <33184065+bofmichal@users.noreply.github.com>
2018-04-20 20:03:13 +1000

    zad 2

--- newjs/newjs.js
+++ newjs/newjs.js
@@ -35,3 +35,30 @@ document.getElementById("wypis4").innerHTML=ar;
 }
 
 //zad 2
+var ard=[];
+function tab5(){
+    //var ar=[];    
+  for (var i = 0; i < 20; ++i) {     
+     ard[i]=" "+(Math.floor(Math.random()*10)+1);
+}
+document.getElementById("wypis5").innerHTML=ard;
+console.log(ard);
+}
+
+function tab6(){ 
+    var tab=ard;
+    tab.reverse(); 
+    
+console.log(tab);
+document.getElementById("wypis6").innerHTML=tab;
+}
+function tab7(){
+    var tabu=ard;
+    for (var i = 0; i < 20; ++i) {
+        //if(i%2=0){
+    tabu.splice(i, 1)
+        //}
+    }
+console.log(tabu);
+document.getElementById("wypis7").innerHTML=tabu;   
+}
